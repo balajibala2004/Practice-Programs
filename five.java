b1df90332cac1083b4f1d2b3eb9f002c457c981d
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-08 03:44:25 -0700

    Add files via upload

---
+++ five.java
@@ -0,0 +1,18 @@
+package java_basics;
+import java.util.*;
+public class five {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Input first number :");
+		Scanner o1 = new Scanner (System.in);
+		int a = o1.nextInt();
+		System.out.println("Input second number :");
+		Scanner o2 = new Scanner (System.in);
+		int b = o2.nextInt();
+		int c = a * b ;
+		System.out.println("result = " + c);
+		
+	}
+
+}
