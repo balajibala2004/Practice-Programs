272c276f91ce7429e6886fdc85c628c6b84d73d7
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-08 03:24:03 -0700

    Add files via upload

---
+++ four.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+public class four {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a." + " " + (-5+8*6));
+		System.out.println("b." + " " + ((55 + 9)%9));
+		System.out.println("c." + " " + (20 + -3 * 5 / 8 ));
+		System.out.println("d." + " " + (+5+ 15 /3 * 2 - 8 % 3));
+
+	}
+
+}
