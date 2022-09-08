453d9ac7c3aec28af3fbd0cdb3b806055f7ca8b1
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-08 04:09:54 -0700

    Add files via upload

---
+++ seven.java
@@ -0,0 +1,20 @@
+package java_basics;
+import java.util.*;
+public class seven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Input a number : ");
+		Scanner o1 = new Scanner(System.in);
+		int a = o1.nextInt();
+		for (int i=1;i<=10;i++)
+		{	
+			int c = a * i ;
+			System.out.println(a + " x " + i +  " = " + c);
+		}
+		
+		
+
+	}
+
+}
