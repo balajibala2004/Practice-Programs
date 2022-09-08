3a235702778efe062d584a50efe49fcba88cb02f
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-08 03:20:44 -0700

    Add files via upload

---
+++ two.java
@@ -0,0 +1,11 @@
+package java_basics;
+
+public class two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = 74 + 36 ;
+		System.out.println(c);
+	}
+
+}
