3f6caacbd54e964490d2bc1ae226c038e585784c
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-08 03:21:58 -0700

    Add files via upload

---
+++ three.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+public class three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = 50 / 3 ;
+		System.out.println(c);
+
+	}
+
+}
