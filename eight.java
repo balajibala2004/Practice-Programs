ed812880b260472a4f2fefa2fd1c08d17bc6014a
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-10 04:23:07 -0700

    Add files via upload

---
+++ eight.java
@@ -0,0 +1,15 @@
+package first;
+
+public class eight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("   J    a   v     v   a");
+		System.out.println("   J   a a   v   v   a a");
+		System.out.println("J  J  aaaaa   v v   aaaaa");
+		System.out.println(" JJ  a     a   v   a     a");
+	}
+
+}
+
+
