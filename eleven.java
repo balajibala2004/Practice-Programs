6d74c2b83216a0cc8aecd9be3174baa80925abc5
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-12 16:54:46 +0530

    Create eleven.java

---
+++ eleven.java
@@ -0,0 +1,21 @@
+package first;
+import java.util.*;
+
+public class temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius,area,perimeter;
+		System.out.println("Enter the radius ");
+		Scanner o = new Scanner(System.in);
+		
+		radius = o.nextDouble();
+		o.close ();
+		area = 3.1415926535897927  * radius *radius ;
+		perimeter =  2 *3.1415926535897927 *radius ;
+		System.out.println(  "area is " + area  );
+		System.out.println("perimeter is" + perimeter);
+		
+	}
+
+}
