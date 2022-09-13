16b1175ae937eff7fbd25976423685a500c33cfd
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2022-09-13 03:47:31 -0700

    Add files via upload

---
+++ fourteen.java
@@ -0,0 +1,21 @@
+
+public class fourteen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0;i<=4;i++)
+		{
+			System.out.println("* * * * * * ==================================");
+			System.out.println(" * * * * *  ==================================");
+		}
+		System.out.println("* * * * * * ==================================");
+		
+		
+		for(int i = 0;i<6;i++)
+		{
+			System.out.println("====================================================");
+		}
+		
+	}
+
+}
