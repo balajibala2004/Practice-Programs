2c023d13b9e2ca005ffd13ead86db93dd92e5499
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2025-08-20 23:09:54 +0530

    Update eight.java

--- eight.java
+++ eight.java
@@ -7,9 +7,11 @@ public class eight {
 		System.out.println("   J    a   v     v   a");
 		System.out.println("   J   a a   v   v   a a");
 		System.out.println("J  J  aaaaa   v v   aaaaa");
+		
 		System.out.println(" JJ  a     a   v   a     a");
 }
 
 }
+
 
 
