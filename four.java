37015458d02791e21fff6765a8c3b4838198587a
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2025-08-23 12:12:13 +0530

    Update four.java

--- four.java
+++ four.java
@@ -7,6 +7,7 @@ public class four {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("a." + " " + (-5+8*6));
+		
 		System.out.println("b." + " " + ((55 + 9)%9));
 		System.out.println("c." + " " + (20 + -3 * 5 / 8 ));
 		System.out.println("d." + " " + (+5+ 15 /3 * 2 - 8 % 3));
@@ -14,4 +15,5 @@ public class four {
 	}
 
 }
+
 
